30d1830138a7f5c5762a3ed66ed839fa3577abb5
conciliacaoautomacao <conciliacao.automacao@gmail.com>
2026-06-17 13:29:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Robô Boletos",
-    page_icon="🤖",
+    page_title="Início",
+    page_icon="🏠",
     layout="wide"
 )
 
-st.title("🤖 Robô de Baixa de Boletos")
-st.info("Acesse o Dashboard no menu lateral para acompanhar o robô.")
+st.title("🏠 Início")
